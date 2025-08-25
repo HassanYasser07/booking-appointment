657d51e6fae5fa8320c868f05bf7844751f5244c
Hassan Yasser <108157502+HassanYasser07@users.noreply.github.com>
2025-08-26 01:28:22 +0300

    update doctor details model

--- lib/fetures/doctor_details/data/models/doctor_details_model.dart
+++ lib/fetures/doctor_details/data/models/doctor_details_model.dart
@@ -3,34 +3,60 @@ import 'package:json_annotation/json_annotation.dart';
 part 'doctor_details_model.g.dart';
 
 @JsonSerializable()
+class DoctorDetailsResponse {
+   String? message;
+   DoctorDetailsModel? data;
+   bool? status;
+   int? code;
+
+  DoctorDetailsResponse({
+    this.message,
+    this.data,
+    this.status,
+    this.code,
+  });
+
+  factory DoctorDetailsResponse.fromJson(Map<String, dynamic> json) =>
+      _$DoctorDetailsResponseFromJson(json);
+
+  Map<String, dynamic> toJson() => _$DoctorDetailsResponseToJson(this);
+}
+
+@JsonSerializable()
 class DoctorDetailsModel {
-   int id;
-   String name;
+   int? id;
+   String? name;
+   String? email;
+   String? phone;
    String? photo;
+   String? gender;
    String? address;
-   String? degree;
-   @JsonKey(name: 'appoint_price')
-   String? price;
-   @JsonKey(name: 'start_time')
-   String? startingTime;
-   @JsonKey(name: 'end_time')
-   String? endingTime;
    String? description;
+   String? degree;
    Specialization? specialization;
    City? city;
+  @JsonKey(name: "appoint_price")
+   num? appointPrice;
+  @JsonKey(name: "start_time")
+   String? startTime;
+  @JsonKey(name: "end_time")
+   String? endTime;
 
   DoctorDetailsModel({
-    required this.id,
-    required this.name,
+    this.id,
+    this.name,
+    this.email,
+    this.phone,
     this.photo,
+    this.gender,
     this.address,
-    this.degree,
-    this.price,
-    this.startingTime,
-    this.endingTime,
     this.description,
+    this.degree,
     this.specialization,
     this.city,
+    this.appointPrice,
+    this.startTime,
+    this.endTime,
   });
 
   factory DoctorDetailsModel.fromJson(Map<String, dynamic> json) =>
@@ -41,10 +67,10 @@ class DoctorDetailsModel {
 
 @JsonSerializable()
 class Specialization {
-   int id;
-   String name;
+   int? id;
+   String? name;
 
-  Specialization({required this.id, required this.name});
+  Specialization({this.id, this.name});
 
   factory Specialization.fromJson(Map<String, dynamic> json) =>
       _$SpecializationFromJson(json);
@@ -54,13 +80,26 @@ class Specialization {
 
 @JsonSerializable()
 class City {
-   int id;
-   String name;
+   int? id;
+   String? name;
+   Governrate? governrate;
 
-  City({required this.id, required this.name});
+  City({this.id, this.name, this.governrate});
 
-  factory City.fromJson(Map<String, dynamic> json) =>
-      _$CityFromJson(json);
+  factory City.fromJson(Map<String, dynamic> json) => _$CityFromJson(json);
 
   Map<String, dynamic> toJson() => _$CityToJson(this);
+}
+
+@JsonSerializable()
+class Governrate {
+   int? id;
+   String? name;
+
+  Governrate({this.id, this.name});
+
+  factory Governrate.fromJson(Map<String, dynamic> json) =>
+      _$GovernrateFromJson(json);
+
+  Map<String, dynamic> toJson() => _$GovernrateToJson(this);
 }
--- lib/fetures/doctor_details/data/models/doctor_details_model.g.dart
+++ lib/fetures/doctor_details/data/models/doctor_details_model.g.dart
@@ -6,17 +6,37 @@ part of 'doctor_details_model.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
+DoctorDetailsResponse _$DoctorDetailsResponseFromJson(
+        Map<String, dynamic> json) =>
+    DoctorDetailsResponse(
+      message: json['message'] as String?,
+      data: json['data'] == null
+          ? null
+          : DoctorDetailsModel.fromJson(json['data'] as Map<String, dynamic>),
+      status: json['status'] as bool?,
+      code: (json['code'] as num?)?.toInt(),
+    );
+
+Map<String, dynamic> _$DoctorDetailsResponseToJson(
+        DoctorDetailsResponse instance) =>
+    <String, dynamic>{
+      'message': instance.message,
+      'data': instance.data,
+      'status': instance.status,
+      'code': instance.code,
+    };
+
 DoctorDetailsModel _$DoctorDetailsModelFromJson(Map<String, dynamic> json) =>
     DoctorDetailsModel(
-      id: (json['id'] as num).toInt(),
-      name: json['name'] as String,
+      id: (json['id'] as num?)?.toInt(),
+      name: json['name'] as String?,
+      email: json['email'] as String?,
+      phone: json['phone'] as String?,
       photo: json['photo'] as String?,
+      gender: json['gender'] as String?,
       address: json['address'] as String?,
-      degree: json['degree'] as String?,
-      price: json['appoint_price'] as String?,
-      startingTime: json['start_time'] as String?,
-      endingTime: json['end_time'] as String?,
       description: json['description'] as String?,
+      degree: json['degree'] as String?,
       specialization: json['specialization'] == null
           ? null
           : Specialization.fromJson(
@@ -24,27 +44,33 @@ DoctorDetailsModel _$DoctorDetailsModelFromJson(Map<String, dynamic> json) =>
       city: json['city'] == null
           ? null
           : City.fromJson(json['city'] as Map<String, dynamic>),
+      appointPrice: json['appoint_price'] as num?,
+      startTime: json['start_time'] as String?,
+      endTime: json['end_time'] as String?,
     );
 
 Map<String, dynamic> _$DoctorDetailsModelToJson(DoctorDetailsModel instance) =>
     <String, dynamic>{
       'id': instance.id,
       'name': instance.name,
+      'email': instance.email,
+      'phone': instance.phone,
       'photo': instance.photo,
+      'gender': instance.gender,
       'address': instance.address,
-      'degree': instance.degree,
-      'appoint_price': instance.price,
-      'start_time': instance.startingTime,
-      'end_time': instance.endingTime,
       'description': instance.description,
+      'degree': instance.degree,
       'specialization': instance.specialization,
       'city': instance.city,
+      'appoint_price': instance.appointPrice,
+      'start_time': instance.startTime,
+      'end_time': instance.endTime,
     };
 
 Specialization _$SpecializationFromJson(Map<String, dynamic> json) =>
     Specialization(
-      id: (json['id'] as num).toInt(),
-      name: json['name'] as String,
+      id: (json['id'] as num?)?.toInt(),
+      name: json['name'] as String?,
     );
 
 Map<String, dynamic> _$SpecializationToJson(Specialization instance) =>
@@ -54,11 +80,26 @@ Map<String, dynamic> _$SpecializationToJson(Specialization instance) =>
     };
 
 City _$CityFromJson(Map<String, dynamic> json) => City(
-      id: (json['id'] as num).toInt(),
-      name: json['name'] as String,
+      id: (json['id'] as num?)?.toInt(),
+      name: json['name'] as String?,
+      governrate: json['governrate'] == null
+          ? null
+          : Governrate.fromJson(json['governrate'] as Map<String, dynamic>),
     );
 
 Map<String, dynamic> _$CityToJson(City instance) => <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+      'governrate': instance.governrate,
+    };
+
+Governrate _$GovernrateFromJson(Map<String, dynamic> json) => Governrate(
+      id: (json['id'] as num?)?.toInt(),
+      name: json['name'] as String?,
+    );
+
+Map<String, dynamic> _$GovernrateToJson(Governrate instance) =>
+    <String, dynamic>{
       'id': instance.id,
       'name': instance.name,
     };
